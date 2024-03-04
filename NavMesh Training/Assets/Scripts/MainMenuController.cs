37e1ae1d7f9b937b4c3d88eb6c462897e1f524a0
kfruda <52004052+BeeAdmin12@users.noreply.github.com>
2024-03-04 13:01:11 -0700

    HowToPlay panel active & QUIT button
    The how to play button now leads to a different panel to tell the player what they need to do when they are playing. It also has a return button so the player can go back to the main menu. Also, forgot to add a quit button, so I added that and gave its functionality

--- NavMesh Training/Assets/Scripts/MainMenuController.cs
+++ NavMesh Training/Assets/Scripts/MainMenuController.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject howToPlayPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,10 +22,22 @@ public class MainMenuController : MonoBehaviour
         
     }
 
+    public void ToggleHowToPlayPanel()
+    {
+        howToPlayPanel.SetActive(!howToPlayPanel.activeSelf);
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
     }
 
-
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
 }
